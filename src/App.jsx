9814c1b562ef34f85678985c47f9fc5a6c6f7915
314125287 <rostyslav.tveritinov@rbc.com>
2017-11-14 16:49:49 -0500

    changes to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,43 @@ class App extends React.Component {
         this.state = {
             todos: this.props.initialData
         }
+
+        this.handleStatusChange = this.handleStatusChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+
     }
+
+    handleStatusChange(id) {
+      let todos = this.state.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+          return todo;
+      });
+      this.setState({todos});
+    }
+
+    handleDelete(id) {
+      let todos = this.state.todos.filter(todo => todo.id !== id);
+
+      this.setState({todos});
+    }
+
     render(){
         return(
-            <main> 
+            <main>
                 <Header title={this.props.title} />
 
                 <section className="todo-list">
-                    {this.state.todos.map(todo => 
-                        <Todo key={todo.id} title={todo.title} completed={todo.completed}/>)
+                    {this.state.todos.map(todo =>
+                        <Todo
+                          key={todo.id}
+                          id={todo.id}
+                          title={todo.title}
+                          completed={todo.completed}
+                          onStatusChange={this.handleStatusChange}
+                          onDelete={this.handleDelete}
+                        />)
                     }
                 </section>
             </main>
@@ -31,7 +59,7 @@ class App extends React.Component {
 App.propTypes = {
     title: React.PropTypes.string,
     initialData: React.PropTypes.arrayOf(React.PropTypes.shape({
-        id: React.PropTypes.number.isRequired,        
+        id: React.PropTypes.number.isRequired,
         title: React.PropTypes.string.isRequired,
         completed: React.PropTypes.bool.isRequired
     })).isRequired
